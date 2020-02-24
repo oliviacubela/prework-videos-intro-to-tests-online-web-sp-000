e7c95baac7240dfe9b5843184a0aee121a532f85
Olivia Cubela <oliviacubela@gmail.com>
2020-02-24 04:06:31 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../conversions.rb'
-puts "Tests are running. . ."
+puts "Tests are running..."
+
 describe "conversions" do
   describe '#ounces_to_grams' do
     it 'given zero, returns 0.0' do
